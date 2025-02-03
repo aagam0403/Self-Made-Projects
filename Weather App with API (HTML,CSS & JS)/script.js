8fbfcde9137cff51647525e26bace246c85b459d
Aagam Sanghvi <aagamsanghvi0403@gmail.com>
2025-02-03 18:02:43 +0530

    Update script.js

--- Weather App with API (HTML,CSS & JS)/script.js
+++ Weather App with API (HTML,CSS & JS)/script.js
@@ -5,7 +5,7 @@ function getWeather() {
 	if (!city) {
 		alert('Please enter a city');
 		return;
-	}
+	} 
 
 	const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 	const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
